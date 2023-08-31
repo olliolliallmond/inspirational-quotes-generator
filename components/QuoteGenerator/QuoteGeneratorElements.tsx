0b60e7ef2c77f9cab9d1b9224d841c5d7e82528c
olliolliallmond <aallmondcar@gmail.com>
2023-08-31 16:36:35 -0400

    Finished initial background

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 export const GradientBackgroundCon = styled.div`
   background: linear-gradient(to right, #000046, #1cb5e0);
   background-size: 400% 400%;
-  animation: gradient 12s ease infinite;
+  animation: gradient 18s ease infinite;
   height: 100vh;
   width: 100vw;
   @keyframes gradient {
@@ -20,4 +20,38 @@ export const GradientBackgroundCon = styled.div`
       background-position: 0% 50%;
     }
   }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: -10px;
+  margin-top: -10px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  right: -120px;
+  bottom: -10px;
+`;
+
+export const FooterCon = styled.div`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-family: 'Source Code Pro', monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  z-index: 999999;
+`;
+
+export const RedSpan = styled.span`
+    color: red;
+`;
+
+export const FooterLink = styled(Link)`
+    color: white;
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+// Styled Components
+import { BackgroundImage1, BackgroundImage2, FooterCon, FooterLink, GradientBackgroundCon, RedSpan } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
+// Assets
+// import Cloud1 from 'assets/sun.jpg'
+import Clouds1 from '@/assets/cloud-and-thunder.png'
+import Clouds2 from '@/assets/cloudy-weather.png'
+import { useState } from 'react'
 
 export default function Home() {
+  // const numberOfQuotes = 8;
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
+  // const handleGenerateQuote = () => {}
+
   return (
     <>
       <Head>
@@ -14,7 +25,26 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
+        <BackgroundImage1
+          src={Clouds1}
+          height="300"
+          alt="cloudybackground1"
+        />
 
+        <BackgroundImage2 
+          src={Clouds2}
+          height="300"
+          alt="cloudybackground1"
+        />
+
+        <FooterCon>
+          {/* Call to Action */}
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with <RedSpan>♥</RedSpan> following this <FooterLink href="https://www.youtube.com/watch?v=FRmCxj9K7II" target="_blank" rel="noopener noreferrer">tutorial</FooterLink>
+          </>
+        </FooterCon>
       </GradientBackgroundCon>
     </>
   )
