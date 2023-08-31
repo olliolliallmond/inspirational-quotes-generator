3f9a7630809995bc4fcc4bd8fca6b91cd763f784
olliolliallmond <aallmondcar@gmail.com>
2023-08-31 15:46:50 -0400

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inspirational Quote Generator</title>
+        <meta name="description" content="A fun project to generate quotes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* Background */}
+      <GradientBackgroundCon>
+
+      </GradientBackgroundCon>
+    </>
+  )
+}
